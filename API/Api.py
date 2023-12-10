3e96b9b7a0493394c3de51743253aab6517d7161
AbukDuot <a.duotalustudent.com>
2023-12-10 03:24:55 -0800

    API

---
+++ API/Api.py
@@ -0,0 +1,48 @@
+import requests
+
+def get_food_data(food_name):
+    url = "https://edamam-food-and-grocery-database.p.rapidapi.com/api/food-database/v2/parser"
+
+    query_params = {
+        "nutrition-type": "cooking",
+        "category[0]": "generic-foods",
+        "health[0]": "alcohol-free",
+        "ingr": food_name  # Add a parameter for the food name
+    }
+
+    headers = {
+        "X-RapidAPI-Key": "9f266e8bd5msh2ef26fbb786638dp1ec1dcjsn95d802f33f18",
+        "X-RapidAPI-Host": "edamam-food-and-grocery-database.p.rapidapi.com"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, params=query_params)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+
+        return response.json()
+    except requests. exceptions.RequestException as e:
+        print(f"Error during API request: {e}")
+        return None
+
+def display_food_info(food_data):
+    if not food_data:
+        print("Failed to retrieve food data.")
+        return
+
+    # Extract relevant information for display
+    food_name = food_data.get('hints', [{}])[0].get('food', {}).get('label', 'Unknown Food')
+    nutrients = food_data.get('hints', [{}])[0].get('food', {}).get('nutrients', {})
+
+    # Display information
+    print(f"Food Name: {food_name}")
+    print("Nutritional Information:")
+    print(f"  - Calories: {nutrients.get('ENERC_KCAL', 'N/A')} kcal")
+    print(f"  - Protein: {nutrients.get('PROCNT', 'N/A')} g")
+    print(f"  - Fat: {nutrients.get('FAT', 'N/A')} g")
+    print(f"  - Carbohydrates: {nutrients.get('CHOCDF', 'N/A')} g")
+
+if __name__ == "__main__":
+    food_name = input("Enter a food name to get nutritional information: ")
+    food_data = get_food_data(food_name)
+
+    display_food_info(food_data)
